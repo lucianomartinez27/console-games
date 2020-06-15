4816a1e6cb22c452b62f4d0a8cbb8d8b35a413d4
Luciano Martínez <lucianomartinez27@gmail.com>
2020-06-15 18:13:05 -0300

    change name ahorcado.py to main.py

---
+++ ahorcado/main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import os
+import random
+from funciones import plantilla_ahorcado
+
+jugando = True
+lista_palabras = "armario caja raton escoba caramelo libro mesa guitarra".split()
+lista_letras = []
+lista_errores = []
+palabra = random.choice(lista_palabras).upper()    
+
+while jugando:
+    print('='* 30)
+    print('EL AHORCADO'.center(30))
+    print('=' * 30)
+    plantilla_ahorcado(palabra, lista_letras, lista_errores)
+    letra = input('\nElige una letra: ').upper()
+    
+    if letra != "":
+        if letra in lista_letras or letra in lista_errores:
+            os.system('clear')
+            print()
+            print('YA HAS ELEGIDO ESA LETRA')
+        elif letra in palabra:
+            lista_letras.append(letra)
+            os.system('clear')
+        else:
+            lista_errores.append(letra)
+            os.system('clear')
+    else:
+        os.system('clear')
+        print()
+        print('INGRESA UNA LETRA')
+
+    if len(lista_errores) == 6:
+        print("=" * 30)
+        print('HAS PERDIDO'.center(30))
+        print(f'LA PALABRA ERA: {palabra}'.center(30))
+        print("=" * 30)
+        jugando = False
+    elif len(lista_letras) == len(set(palabra)):
+        print("=" * 30)
+        print('ACERTASTE, LA PALABRA ERA:'.center(30))
+        print(f"****** {palabra} ******".center(30))
+        print("HAS GANADO".center(30))
+        print("=" * 30)
+        jugando = False
